4e884e4db05548f484833b50f6c2ac8ed660fbde
shehab <ibrahimshehab2034@gmail.com>
2024-09-02 10:25:13 +0200

    finishing Sooth App2

--- app/src/main/java/com/example/my/AlignBodyElement/AlignBodyElement.kt
+++ app/src/main/java/com/example/my/AlignBodyElement/AlignBodyElement.kt
@@ -40,7 +40,10 @@ val bodyItems = listOf(
     R.drawable.ab4_tabata to R.string.ab4_tabata,
     R.drawable.ab3_stretching to R.string.ab3_stretching,
     R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
-    R.drawable.ab1_inversions to R.string.ab1_inversions
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
 ).map { BodyData(it.first, it.second) }
 
 @Composable
